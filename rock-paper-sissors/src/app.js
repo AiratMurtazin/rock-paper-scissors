b84c983f0db8da5a33d62ec23fbfb41e24229fab
Airat Murtazin <ayrat.murtazin@mail.ru>
2022-11-03 12:27:47 +0300

    init: create html and app.js files

---
+++ rock-paper-sissors/src/app.js
@@ -0,0 +1,9 @@
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection. )
+}
+function getComputerChoice() {
+let arr = ["Rock", "Paper", "Scissors"];
+let random = Math.floor(Math.random() * arr.length);
+console.log(arr[random]);
+}
+getComputerChoice();
